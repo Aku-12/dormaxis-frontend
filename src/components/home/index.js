b3d8073587c71b4b3fec22f32444a4b0740669fb
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-11 17:32:10 +0545

    Export home components

---
+++ src/components/home/index.js
@@ -0,0 +1,7 @@
+export { default as HeroSection } from './HeroSection';
+export { default as FeaturesSection } from './FeaturesSection';
+export { default as PopularDormsSection } from './PopularDormsSection';
+export { default as WhyChooseSection } from './WhyChooseSection';
+export { default as CTASection } from './CTASection';
+export { default as DormCard } from './DormCard';
+export { default as FeatureCard } from './FeatureCard';

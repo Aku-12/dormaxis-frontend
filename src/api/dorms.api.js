a275d8b92ad5bdddb3e35c6a076d521039d7d2de
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-12 15:45:46 +0545

    Add dorms API methods

---
+++ src/api/dorms.api.js
@@ -0,0 +1,55 @@
+import axiosClient from './axios.client';
+import { API_ENDPOINTS } from '../config/api.config';
+
+export const dormsAPI = {
+  /**
+   * Get all dorms with optional filters
+   * @param {Object} filters - Filter parameters (search, minPrice, maxPrice, beds, block)
+   * @returns {Promise} Response with dorms array
+   */
+  getAllDorms: async (filters = {}) => {
+    const response = await axiosClient.get(API_ENDPOINTS.DORMS, { params: filters });
+    return response.data;
+  },
+
+  /**
+   * Get single dorm by ID
+   * @param {string} id - Dorm ID
+   * @returns {Promise} Response with dorm data
+   */
+  getDormById: async (id) => {
+    const response = await axiosClient.get(API_ENDPOINTS.DORM_BY_ID(id));
+    return response.data;
+  },
+
+  /**
+   * Create new dorm
+   * @param {Object} dormData - Dorm data
+   * @returns {Promise} Response with created dorm
+   */
+  createDorm: async (dormData) => {
+    const response = await axiosClient.post(API_ENDPOINTS.DORMS, dormData);
+    return response.data;
+  },
+
+  /**
+   * Update dorm
+   * @param {string} id - Dorm ID
+   * @param {Object} dormData - Updated dorm data
+   * @returns {Promise} Response with updated dorm
+   */
+  updateDorm: async (id, dormData) => {
+    const response = await axiosClient.put(API_ENDPOINTS.DORM_BY_ID(id), dormData);
+    return response.data;
+  },
+
+  /**
+   * Delete dorm
+   * @param {string} id - Dorm ID
+   * @returns {Promise} Response with success message
+   */
+  deleteDorm: async (id) => {
+    const response = await axiosClient.delete(API_ENDPOINTS.DORM_BY_ID(id));
+    return response.data;
+  },
+};

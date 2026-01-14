6d5c2c524238740c498fcee215510067944516d3
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-14 14:21:21 +0545

    Add application constants

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+// App Constants
+
+export const APP_NAME = 'DormAxis';
+
+export const SOCIAL_LINKS = {
+  FACEBOOK: '#',
+  TWITTER: '#',
+  INSTAGRAM: '#',
+  LINKEDIN: '#',
+};
+
+export const CONTACT_INFO = {
+  EMAIL: 'info@dormaxis.com',
+  PHONE: '+977 123-456-789',
+  ADDRESS: 'Kathmandu, Nepal',
+};
+
+export const NAV_LINKS = [
+  { id: 1, name: 'Home', href: '#home' },
+  { id: 2, name: 'Dorms', href: '#dorms' },
+  { id: 3, name: 'Contact Us', href: '#contact' },
+  { id: 4, name: 'About Us', href: '#about' },
+];

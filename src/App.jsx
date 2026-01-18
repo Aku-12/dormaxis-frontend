f987aed1d767b9509dd56c5c870e18e05140cb84
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-18 23:04:24 +0545

    Update app routing and layout integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import DormsPage from './pages/DormsPage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
+import ForgotPasswordPage from './pages/ForgotPasswordPage'
+import ProfilePage from './pages/ProfilePage'
 import { AdminLayout, AdminProtectedRoute } from './components/admin'
 import { AdminDashboard, AdminDorms, AdminUsers } from './pages/admin'
 import useAuthStore from './store/useAuthStore'
@@ -18,8 +21,18 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dorms" element={<DormsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
 
         {/* Admin Routes */}
         <Route

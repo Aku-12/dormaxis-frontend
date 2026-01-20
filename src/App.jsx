e848465219b807279ae70c754e68bdbf5e81690e
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-20 22:01:38 +0545

    Update application routing and integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import DormsPage from './pages/DormsPage'
+import DormDetailPage from './pages/DormDetailPage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
 import ProfilePage from './pages/ProfilePage'
+import WishlistPage from './pages/WishlistPage'
+import BookingPage from './pages/BookingPage'
+import BookingSuccessPage from './pages/BookingSuccessPage'
+import PaymentPage from './pages/PaymentPage'
+import PaymentSuccessPage from './pages/PaymentSuccessPage'
+import PaymentFailurePage from './pages/PaymentFailurePage'
+import KhaltiCallbackPage from './pages/KhaltiCallbackPage'
 import { AdminLayout, AdminProtectedRoute } from './components/admin'
 import { AdminDashboard, AdminDorms, AdminUsers } from './pages/admin'
+import { ToastProvider } from './components/common'
 import useAuthStore from './store/useAuthStore'
 
 // Protected Route Component
@@ -18,13 +27,70 @@ const ProtectedRoute = ({ children }) => {
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dorms" element={<DormsPage />} />
+    <ToastProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dorms" element={<DormsPage />} />
+          <Route path="/dorms/:id" element={<DormDetailPage />} />
+          <Route
+            path="/wishlist"
+            element={
+              <ProtectedRoute>
+                <WishlistPage />
+              </ProtectedRoute>
+            }
+          />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route
+          path="/booking/:dormId"
+          element={
+            <ProtectedRoute>
+              <BookingPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/booking/success/:bookingId"
+          element={
+            <ProtectedRoute>
+              <BookingSuccessPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/payment"
+          element={
+            <ProtectedRoute>
+              <PaymentPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/payment/success"
+          element={
+            <ProtectedRoute>
+              <PaymentSuccessPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/payment/failure"
+          element={
+            <ProtectedRoute>
+              <PaymentFailurePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/payment/khalti/callback/:bookingId"
+          element={
+            <ProtectedRoute>
+              <KhaltiCallbackPage />
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/profile"
           element={
@@ -48,9 +114,10 @@ function App() {
           <Route path="users" element={<AdminUsers />} />
         </Route>
 
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </ToastProvider>
   )
 }
 

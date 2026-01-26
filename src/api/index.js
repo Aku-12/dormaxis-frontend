8e377a67b08f50790fe57e0d561fe1ddf6f097f6
Aku-12 <akashchaudhary205698@gmail.com>
2026-01-26 15:15:42 +0545

    Update API module exports

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 export { homeAPI } from './home.api';
 export { dormsAPI, wishlistAPI, reviewsAPI } from './dorms.api';
 export { authAPI } from './auth.api';
+export { mfaAPI } from './mfa.api';
 export { bookingAPI } from './booking.api';
 export { paymentAPI } from './payment.api';
 export { default as axiosClient } from './axios.client';
+export { reviewAPI } from './review.api';
+export { notificationAPI } from './notification.api';
